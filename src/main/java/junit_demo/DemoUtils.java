db4fa26f39f7da4262df64a5041cf6f96ac3f760
Sergey Makarichev <harleymack@mail.ru>
2022-10-20 17:56:45 +0300

    same/true/structures tests

--- src/main/java/junit_demo/DemoUtils.java
+++ src/main/java/junit_demo/DemoUtils.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class DemoUtils {
 
-    private String academy = "Luv2Code Academy";
-    private String academyDuplicate = academy;
-    private String[] firstThreeLettersOfAlphabet = {"A", "B", "C"};
-    private List<String> academyInList = List.of("luv", "2", "code");
+    private final String academy = "Luv2Code Academy";
+    private final String academyDuplicate = academy;
+    private final String[] firstThreeLettersOfAlphabet = {"A", "B", "C"};
+    private final List<String> academyInList = List.of("luv", "2", "code");
 
     public List<String> getAcademyInList() {
         return academyInList;
@@ -34,17 +34,11 @@ public class DemoUtils {
     }
 
     public Object checkNull(Object obj) {
-        if (obj != null) {
-            return obj;
-        }
-        return null;
+        return obj;
     }
 
     public Boolean isGreater(int n1, int n2) {
-        if (n1 > n2) {
-            return true;
-        }
-        return false;
+        return n1 > n2;
     }
 
     public String throwException(int a) throws Exception {
@@ -59,5 +53,4 @@ public class DemoUtils {
         Thread.sleep(2000);
         System.out.println("Sleeping over");
     }
-
 }
--- src/test/java/junit_demo/DemoUtilsTest.java
+++ src/test/java/junit_demo/DemoUtilsTest.java
@@ -2,9 +2,10 @@ package junit_demo;
 
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-//@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 class DemoUtilsTest {
 
     private DemoUtils demoUtils;
@@ -33,7 +34,7 @@ class DemoUtilsTest {
     }
 
     @Test
-    @DisplayName("Checking for equals / not equals")
+    @DisplayName("Equals / not equals")
     void testAdd() {
 //        System.out.println("Running test: testAdd");
         assertEquals(6, demoUtils.add(2, 4));
@@ -41,12 +42,39 @@ class DemoUtilsTest {
     }
 
     @Test
-    @DisplayName("Checking for null / not null")
+    @DisplayName("is null / not null")
     void testCheckNull() {
 //        System.out.println("Running test: testCheckNull");
         String str1 = null;
         String str2 = "test";
         assertNull(demoUtils.checkNull(str1), "should be null");
         assertNotNull(demoUtils.checkNull(str2), "should not be null");
+    }
+
+    @Test
+    @DisplayName("Is same / not same")
+    void testSameWord() {
+        String str = "Luv2Code";
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate());
+        assertNotSame(str, demoUtils.getAcademyDuplicate());
+    }
+
+    @Test
+    @DisplayName("Is bigger value")
+    void testTrueBig() {
+        int grade1 = 10;
+        int grade2 = 5;
+        assertTrue(demoUtils.isGreater(grade1, grade2), "Should return true");
+        assertFalse(demoUtils.isGreater(grade2, grade1), "Should return false");
+    }
+
+    @Test
+    @DisplayName("Arrays/Iterables/Strings equality")
+    void testStructuresEquality() {
+        String[] arr = {"A", "B", "C"};
+        List<String> str = List.of("luv", "2", "code");
+        assertArrayEquals(arr, demoUtils.getFirstThreeLettersOfAlphabet());
+        assertIterableEquals(str, demoUtils.getAcademyInList());
+        assertLinesMatch(str, demoUtils.getAcademyInList());
     }
 }

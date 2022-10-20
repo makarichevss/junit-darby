4e0a3192ba52573fd39536432c2feb0660a7be7f
Sergey Makarichev <harleymack@mail.ru>
2022-10-20 16:54:11 +0300

    various before/after tests with custom names

--- src/test/java/junit_demo/DemoUtilsTest.java
+++ src/test/java/junit_demo/DemoUtilsTest.java
@@ -1,25 +1,49 @@
 package junit_demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class DemoUtilsTest {
+//@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
+class DemoUtilsTest {
 
+    private DemoUtils demoUtils;
 
-    private DemoUtils demoUtils = new DemoUtils();
+    @BeforeAll
+    static void setupBeforeAllTests() {
+        System.out.println("@BeforeAll execution\n");
+    }
 
+    @AfterAll
+    static void setupAfterAllTests() {
+        System.out.println("@BeforeAll execution");
+    }
 
+    @BeforeEach
+    void setupCreateTestObject() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach execution");
+    }
+
+    @AfterEach
+    void setupDeleteTestObject() {
+        demoUtils = new DemoUtils();
+        System.out.println("@AfterEach execution\n");
+
+    }
 
     @Test
+    @DisplayName("Checking for equals / not equals")
     void testAdd() {
+//        System.out.println("Running test: testAdd");
         assertEquals(6, demoUtils.add(2, 4));
         assertNotEquals(7, demoUtils.add(2, 4));
     }
 
     @Test
+    @DisplayName("Checking for null / not null")
     void testCheckNull() {
-        DemoUtils demoUtils = new DemoUtils();
+//        System.out.println("Running test: testCheckNull");
         String str1 = null;
         String str2 = "test";
         assertNull(demoUtils.checkNull(str1), "should be null");
